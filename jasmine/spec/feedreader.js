e2907382fa49d4cf023539bbde558f712a56065a
Oséias Paulo da Silva <hoseaps@gmail.com>
2018-10-17 22:05:30 -0300

    adding last test, that check content changes when new feed is loaded

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -81,18 +81,52 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
+    
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+			loadFeed(0, function() {
+				done();
+			});
+        });
 
+        it('must have at least a single .entry element after loadFeed is called', function(done) {
+			const entries = document.querySelectorAll('.feed .entry');
+            expect(!!entries[0]).toBe(true);
+            done();
+		});
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+        let firstFeed,
+            newFeed;
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                firstFeed = document.querySelector('.feed').innerHTML;
+
+                loadFeed(1, function() {
+                    newFeed = document.querySelector('.feed').innerHTML;
+                    done();
+                });
+            });
+        });
+    
+        it('changes the content when new feed is loaded by the loadFeed', function(done) {
+            expect(newFeed).not.toBe(firstFeed);
+            done();
+        });
+    });
 }());
